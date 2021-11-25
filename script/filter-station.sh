4a0c6c5274a41d79303bb423861a447103a714e3
TAKEUCHI Yoshitaka <yosh@baud.jp>
2021-11-25 11:22:57 +0900

    駅の重複を削除

--- script/filter-station.sh
+++ script/filter-station.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat $(dirname $0)/../data/station20210312free.csv | awk -F',' '{ if ( $7 == 13 ) print $3,$10,$11}' >> $(dirname $0)/../data/station-coord
+cat $(dirname $0)/../data/station20210312free.csv | awk -F',' '{ if ( $7 == 13 && !stationname[$3]++ ) print $3,$10,$11}' > $(dirname $0)/../data/station-coord
