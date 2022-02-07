722c0ab84fad2bf54615a4cdbf46e1cb42d59138
TAKEUCHI Yoshitaka <yosh@baud.jp>
2022-02-07 20:38:41 +0900

    zoom

--- src/getimage.rb
+++ src/getimage.rb
@@ -17,7 +17,7 @@ session = Selenium::WebDriver.for(
 )
 session.manage.timeouts.implicit_wait = 20
 
-session.navigate.to("https://maps.gsi.go.jp/#15/#{answer[2]}/#{answer[1]}/&ls=experimental_rdcl%7Cexperimental_railcl%7Cexperimental_rvrcl&disp=111")
+session.navigate.to("https://maps.gsi.go.jp/#16/#{answer[2]}/#{answer[1]}/&ls=experimental_rdcl%7Cexperimental_railcl%7Cexperimental_rvrcl&disp=111")
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_CONSUMER_TOKEN']
@@ -26,7 +26,7 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
-p "https://maps.gsi.go.jp/#15/#{answer[2]}/#{answer[1]}/&ls=experimental_rdcl%7Cexperimental_railcl%7Cexperimental_rvrcl&disp=111"
+p "https://maps.gsi.go.jp/#16/#{answer[2]}/#{answer[1]}/&ls=experimental_rdcl%7Cexperimental_railcl%7Cexperimental_rvrcl&disp=111"
 sleep 10
 session.save_screenshot('./result.png')
 client.update_with_media("どこだどこだ", open('./result.png'))
